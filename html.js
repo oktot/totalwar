20dd2e9c813160564b284aa8465659d5cd63c2f6
Odayaka <37972067+odayakabe@users.noreply.github.com>
2018-04-08 10:39:49 +0700

    thay the yeume1090 bang thanhmai.ads
    thay the yeume1090 bang thanhmai.ads adsense register ngay 8.4

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-6680370057990805',
+    google_ad_client: 'ca-pub-4930721531480234',
     enable_page_level_ads: true
   });
   `;
